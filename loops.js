cfb8fa2e84a5286d464367a1557d786f4956a144
Cara Morelli <caramorelli7@gmail.com>
2018-08-20 01:06:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
     if (i > 0) {
       array.push("I am " + i + " strange loops.");
     }
-    else if (i === 0) {
+    else if (i === 1) {
       array.push("I am " + i + " strange loop.");
     }
   }
